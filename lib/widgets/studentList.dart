5e77ee5311cb2d52a8869aa70c1bc0c2f2f76493
Victor Del Carpio <vhdelcg@gmail.com>
2020-09-22 01:49:27 -0400

    Create studentList.dart

---
+++ lib/widgets/studentList.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:yalies/models/studentModel.dart';
+import 'package:yalies/services/webservice.dart';
+
+class StudentListState extends State<StudentList> {
+  List<StudentModel> _studentModels = List<StudentModel>();
+
+  @override
+  void initState() {
+    super.initState();
+    _populateStudentModels();
+  }
+
+  void _populateStudentModels() {
+    Webservice().load(StudentModel.all).then((studentModels) => {
+          setState(() => {_studentModels = studentModels})
+        });
+  }
+
+  ListTile _buildItemsForListView(BuildContext context, int index) {
+    return ListTile(
+      title: Image.network(_studentModels[index].image),
+      subtitle: Text(
+          _studentModels[index].first_name +
+              " " +
+              _studentModels[index].last_name,
+          style: TextStyle(fontSize: 18)),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Yalies.io'),
+        ),
+        body: ListView.builder(
+          itemCount: _studentModels.length,
+          itemBuilder: _buildItemsForListView,
+        ));
+  }
+}
+
+class StudentList extends StatefulWidget {
+  @override
+  createState() => StudentListState();
+}
