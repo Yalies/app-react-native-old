5fd4ab2f5c34670e5cddde5b63bc16b7063e97b9
ErikBoesen <me@erikboesen.com>
2020-09-28 22:58:13 -0600

    Create multiple activity widgets

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,39 @@ import 'package:yalies/widgets/studentList.dart';
 void main() => runApp(App());
 
 class App extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: "Yalies",
-        theme: ThemeData(primaryColor: Color.fromRGBO(0, 53, 107, 1)),
-        home: StudentList());
-  }
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            title: "Yalies",
+            theme: ThemeData(primaryColor: Color.fromRGBO(0, 53, 107, 1)),
+            home: MainWidget()
+        );
+    }
+}
+
+class MainWidget extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        Widget widget;
+        // TODO: check login status
+        widget = LoginWidget();
+        return widget;
+    }
+}
+
+class LoginWidget extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        return WebView(
+            initialUrl: 'https://flutter.io',
+            javascriptMode: JavascriptMode.unrestricted,
+        );
+    }
+}
+
+class HomeWidget extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        return StudentList();
+    }
 }
