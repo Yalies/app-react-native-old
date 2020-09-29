38fe37076d4a026b1f5cb43a7cf9aaebe1baccdd
ErikBoesen <me@erikboesen.com>
2020-09-29 00:02:18 -0600

    Fix indentation in studentList code

--- lib/widgets/studentList.dart
+++ lib/widgets/studentList.dart
@@ -4,42 +4,43 @@ import 'package:yalies/models/student.dart';
 import 'package:yalies/services/api.dart';
 
 class StudentListState extends State<StudentList> {
-  List<Student> _students = List<Student>();
+    List<Student> _students = List<Student>();
 
-  @override
-  void initState() {
-    super.initState();
-    _populateStudents();
-  }
+    @override
+    void initState() {
+        super.initState();
+        _populateStudents();
+    }
 
-  void _populateStudents() {
-    API().load(Student.all).then((students) => {
-          setState(() => {_students = students})
+    void _populateStudents() {
+        API().load(Student.all).then((students) => {
+            setState(() => {_students = students})
         });
-  }
+    }
 
-  ListTile _buildItemsForListView(BuildContext context, int index) {
-    return ListTile(
-      title: Image.network(_students[index].image),
-      subtitle: Text(
-          _students[index].first_name +
-              " " +
-              _students[index].last_name,
-          style: TextStyle(fontSize: 18)),
-    );
-  }
+    ListTile _buildItemsForListView(BuildContext context, int index) {
+        return ListTile(
+            title: Image.network(_students[index].image),
+            subtitle: Text(
+                _students[index].first_name +
+                " " +
+                _students[index].last_name,
+            style: TextStyle(fontSize: 18)),
+        );
+    }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Yalies.io'),
-        ),
-        body: ListView.builder(
-          itemCount: _students.length,
-          itemBuilder: _buildItemsForListView,
-        ));
-  }
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            appBar: AppBar(
+                title: Text('Yalies.io'),
+            ),
+            body: ListView.builder(
+                itemCount: _students.length,
+                itemBuilder: _buildItemsForListView,
+            )
+        );
+    }
 }
 
 class StudentList extends StatefulWidget {
